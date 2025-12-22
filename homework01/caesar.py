acdda7d14d4975beb76031557ec3be11a3c7be3a
Furyondx <duchinkirill@gmail.com>
2025-12-23 00:19:07 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -43,6 +43,7 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
 
     return plaintext
 
+git commit -m "Реализована функция encrypt_caesar()"
 
 print(encrypt_caesar("ADOlfIKuSmaXiMus3.6"))
 print(decrypt_caesar("DGRoiLNxVpdAlPxv3.6"))
